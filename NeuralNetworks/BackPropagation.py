dac1fa5cb2660b43afc92fb25789df94f72083ba
rmEleven <1569323975@qq.com>
2023-04-30 18:11:55 +0800

    添加注释

--- NeuralNetworks/BackPropagation.py
+++ NeuralNetworks/BackPropagation.py
@@ -7,148 +7,184 @@ from sklearn.metrics import classification_report
 
 
 def load_data(path, transpose=True):
-    data = sio.loadmat(path)
-    y = data.get('y')  # (5000,1)
-    y = y.reshape(y.shape[0])
+    '''
+    加载数据的函数
+    '''
+    data = sio.loadmat(path)   # 读取MATLAB格式的数据文件
 
-    X = data.get('X')  # (5000,400)
+    X = data.get('X')          # 从加载的数据中获取X变量，它是一个形状为(5000, 400)的二维numpy数组。
+    y = data.get('y')          # 从加载的数据中获取y变量，它是一个形状为(5000, 1)的二维numpy数组。
+    y = y.reshape(y.shape[0])  # 将y变量从二维数组改变为一维数组，使其形状为(5000,)。
 
     if transpose:
+        '''将X变量转置 转置前后形状不变 仍是(400,)的一维数组'''
+        # 将每个样本的数据从(400,)的一维数组形式重构为(20, 20)的二维数组形式,然后再转置
         X = np.array([im.reshape((20, 20)).T for im in X])
+        # 将每个样本的数据从(20, 20)的二维数组形式重构为(400,)的一维数组形式。
         X = np.array([im.reshape(400) for im in X])
 
-    return X, y
+    return X, y  # 返回处理后的特征向量矩阵X和标签向量y。
 
 def load_weight(path):
-    data = sio.loadmat(path)
-    return data['Theta1'], data['Theta2']
+    '''
+    加载神经网络训练好的权重数据
+    '''
+    data = sio.loadmat(path)               # 读取MATLAB格式的数据文件
+    return data['Theta1'], data['Theta2']  # 从读取的数据中取出权重参数
 
 def plot_100_image(X):
-    """ sample 100 image and show them
-    assume the image is square
-
+    '''
+    随机从数据集中抽取100张图片进行展示
     X : (5000, 400)
-    """
-    size = int(np.sqrt(X.shape[1]))
+    '''
+    size = int(np.sqrt(X.shape[1]))  # 计算图像的边长
 
-    # sample 100 image, reshape, reorg it
-    sample_idx = np.random.choice(np.arange(X.shape[0]), 100)  # 100*400
+    # 从数据集中随机抽取100张图片
+    sample_idx = np.random.choice(np.arange(X.shape[0]), 100)  # 100 * 400
+    # 对抽取的100张图片进行重新排列
     sample_images = X[sample_idx, :]
 
+    # 绘制10x10的网格，将100张图片排列在网格中
     fig, ax_array = plt.subplots(nrows=10, ncols=10, sharey=True, sharex=True, figsize=(8, 8))
 
+    # 对于每一个网格，绘制一张图片
     for r in range(10):
         for c in range(10):
-            ax_array[r, c].matshow(sample_images[10 * r + c].reshape((size, size)),
-                                   cmap=matplotlib.cm.binary)
+            # 使用matshow绘制图片
+            ax_array[r, c].matshow(sample_images[10 * r + c].reshape((size, size)), cmap=matplotlib.cm.binary)
+            # 隐藏刻度
             plt.xticks(np.array([]))
             plt.yticks(np.array([]))
 
 def plot_hidden_layer(theta):
-    """
+    '''
+    展示神经网络第一层的权重矩阵
     theta: (10285, )
-    """
-    final_theta1, _ = deserialize(theta)
-    hidden_layer = final_theta1[:, 1:]  # ger rid of bias term theta
+    '''
+    final_theta1, _ = deserialize(theta)  # 将一维数组形式的theta还原成二维矩阵形式的theta1
+    hidden_layer = final_theta1[:, 1:]    # 获取去掉偏置项后的第一层权重矩阵
 
+    # 创建5*5的画布，用于展示25个隐藏层单元的权重
     fig, ax_array = plt.subplots(nrows=5, ncols=5, sharey=True, sharex=True, figsize=(5, 5))
 
+    # 逐个展示隐藏层单元的权重
     for r in range(5):
         for c in range(5):
-            ax_array[r, c].matshow(hidden_layer[5 * r + c].reshape((20, 20)),
-                                   cmap=matplotlib.cm.binary)
+            # 将该单元的权重矩阵转换成20*20的矩阵，并绘制为灰度图
+            ax_array[r, c].matshow(hidden_layer[5 * r + c].reshape((20, 20)), cmap=matplotlib.cm.binary)
+            # 设置坐标轴刻度为空
             plt.xticks(np.array([]))
             plt.yticks(np.array([]))
 
 def expand_y(y):
-    """
-    expand 5000*1 into 5000*10
-    where y=10 -> [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]: ndarray
-    """
-    res = []
+    '''
+    将原始的标签y进行扩展，将其转化为一个维度为5000x10的矩阵，
+    其中每一行对应一个样本的标签，每行有10个元素，表示对应数字的概率，
+    对于真实的标签对应的位置，为1，其他位置为0。
+    '''
+    res = []  # 用于存储转换后的输出结果
+
     for i in y:
-        y_array = np.zeros(10)
-        y_array[i - 1] = 1
+        y_array = np.zeros(10)  # 初始化一行的输出结果为全 0 数组
+        y_array[i - 1] = 1      # 将对应数字位置的值设置为 1
 
-        res.append(y_array)
+        res.append(y_array)     # 将一行的输出结果添加到 res 中
 
-    return np.array(res)
+    return np.array(res)        # 将结果转换为 numpy 数组并返回
 
-# from sklearn.preprocessing import OneHotEncoder
-# encoder = OneHotEncoder(sparse=False)
-# y_onehot = encoder.fit_transform(y)
-# y_onehot.shape #这个函数与expand_y(y)一致
+    # from sklearn.preprocessing import OneHotEncoder
+    # encoder = OneHotEncoder(sparse=False)
+    # y_onehot = encoder.fit_transform(y)
+    # y_onehot.shape #这个函数与expand_y(y)一致
 
 def expand_array(arr):
-    """replicate array into matrix
+    '''
+    将一个一维数组复制成一个矩阵，行数等于数组的长度。
     [1, 2, 3]
 
     [[1, 2, 3],
      [1, 2, 3],
      [1, 2, 3]]
-    """
-    # turn matrix back to ndarray
+    '''
+    # 全为1，长度为数组长度的列向量 @ 原始数组构成的矩阵
     return np.array(np.matrix(np.ones(arr.shape[0])).T @ np.matrix(arr))
 
 def serialize(a, b):
-    return np.concatenate((np.ravel(a), np.ravel(b)))
+    '''
+    将神经网络参数进行序列化（编码）
+    '''
+    return np.concatenate((np.ravel(a), np.ravel(b)))  # 将两个参数a和b沿着水平方向拼接成一个一维向量
 
 def deserialize(seq):
-#     """into ndarray of (25, 401), (10, 26)"""
-    return seq[:25 * 401].reshape(25, 401), seq[25 * 401:].reshape(10, 26)
+    '''
+    将神经网络参数进行反序列化（解码）
+    '''
+    return seq[:25 * 401].reshape(25, 401), seq[25 * 401:].reshape(10, 26)  # 将一个一维向量解码成两个矩阵
 
 def sigmoid(z):
+    '''
+    逻辑函数
+    '''
     return 1 / (1 + np.exp(-z))
 
 def sigmoid_gradient(z):
+    '''
+    逻辑函数的导数
+    '''
     return np.multiply(sigmoid(z), 1 - sigmoid(z))
 
 def feed_forward(theta, X):
-    """apply to architecture 400+1 * 25+1 *10
-    X: 5000 * 401
-    """
-
+    '''
+    对神经网络进行前向传播计算
+    '''
     t1, t2 = deserialize(theta)  # t1: (25,401) t2: (10,26)
-    m = X.shape[0]
-    a1 = X  # 5000 * 401
+    m = X.shape[0]  # 样本数
 
+    # 输入层
+    a1 = X          # 5000 * 401
+
+    # 隐藏层
     z2 = a1 @ t1.T  # 5000 * 25
-    a2 = np.insert(sigmoid(z2), 0, np.ones(m), axis=1)  # 5000*26
+    a2 = np.insert(sigmoid(z2), 0, np.ones(m), axis=1)  # 5000 * 26
 
-    z3 = a2 @ t2.T  # 5000 * 10
-    h = sigmoid(z3)  # 5000*10, this is h_theta(X)
+    # 输出层
+    z3 = a2 @ t2.T   # 5000 * 10
+    h = sigmoid(z3)  # 5000 * 10
 
-    # return的这些均是后向传播所需要的
     return a1, z2, a2, z3, h
 
 def cost(theta, X, y):
-    """
-    y: (m, k) ndarray
-    """
-    m = X.shape[0]  # get the data size m
+    '''
+    计算神经网络的代价函数
+    '''
+    m = X.shape[0]  # 样本数
 
-    _, _, _, _, h = feed_forward(theta, X)
+    _, _, _, _, h = feed_forward(theta, X)  # 前向传播
 
-    # np.multiply is pairwise operation
+    # 计算代价函数
+    # np.multiply 是逐个元素相乘的操作
     pair_computation = -np.multiply(y, np.log(h + 1e-10)) - np.multiply((1 - y), np.log(1 - h + 1e-10))
-
     return pair_computation.sum() / m
 
 def regularized_cost(theta, X, y, lambd=1):
-    """the first column of t1 and t2 is intercept theta, ignore them when you do regularization"""
+    '''
+    计算神经网络的正则化代价函数
+    '''
     t1, t2 = deserialize(theta)  # t1: (25,401) t2: (10,26)
-    m = X.shape[0]
+    m = X.shape[0]  # 样本数
 
-    reg_t1 = (lambd / (2 * m)) * np.power(t1[:, 1:], 2).sum()  # this is how you ignore first col
-    reg_t2 = (lambd / (2 * m)) * np.power(t2[:, 1:], 2).sum()
+    reg_t1 = (lambd / (2 * m)) * np.power(t1[:, 1:], 2).sum()  # 计算正则项
+    reg_t2 = (lambd / (2 * m)) * np.power(t2[:, 1:], 2).sum()  # 计算正则项
 
     return cost(theta, X, y) + reg_t1 + reg_t2
 
 def gradient(theta, X, y):
-    # initialize
+    '''
+    计算神经网络的反向传播梯度
+    '''
+    # 初始化
     t1, t2 = deserialize(theta)  # t1: (25,401) t2: (10,26)
-    m = X.shape[0]
-
+    m = X.shape[0]               # 样本数
     delta1 = np.zeros(t1.shape)  # (25, 401)
     delta2 = np.zeros(t2.shape)  # (10, 26)
 
@@ -163,15 +199,15 @@ def gradient(theta, X, y):
         hi = h[i, :]    # (1, 10)
         yi = y[i, :]    # (1, 10)
 
-        # 第3层
+        # 第3层 输出层
         d3i = hi - yi  # (1, 10)
 
-        # 第2层
+        # 第2层 隐藏层
         z2i = np.insert(z2i, 0, np.ones(1))  # make it (1, 26) to compute d2i
         d2i = np.multiply(t2.T @ d3i, sigmoid_gradient(z2i))  # (1, 26)
 
-        # careful with np vector transpose
-        delta2 += np.matrix(d3i).T @ np.matrix(a2i)  # (1, 10).T @ (1, 26) -> (10, 26)
+        # 反向传播
+        delta2 += np.matrix(d3i).T @ np.matrix(a2i)      # (1, 10).T @ (1, 26) -> (10, 26)
         delta1 += np.matrix(d2i[1:]).T @ np.matrix(a1i)  # (1, 25).T @ (1, 401) -> (25, 401)
 
     delta1 = delta1 / m
@@ -179,72 +215,61 @@ def gradient(theta, X, y):
 
     return serialize(delta1, delta2)
 
-def regularized_gradient(theta, X, y, l=1):
-    """don't regularize theta of bias terms"""
-    m = X.shape[0]
-    delta1, delta2 = deserialize(gradient(theta, X, y))
-    t1, t2 = deserialize(theta)
+def regularized_gradient(theta, X, y, lambd=1):
+    '''
+    计算神经网络的反向传播正则化梯度
+    '''
+    m = X.shape[0]  # 样本数
+    delta1, delta2 = deserialize(gradient(theta, X, y))  # 不带正则项的梯度
+    t1, t2 = deserialize(theta)  # 解码为矩阵
 
-    t1[:, 0] = 0
-    reg_term_d1 = (l / m) * t1
-    delta1 = delta1 + reg_term_d1
+    t1[:, 0] = 0                    # 对偏置项的theta进行0处理，不参与正则化
+    reg_term_d1 = (lambd / m) * t1  # 计算正则化项对第一层的影响
+    delta1 = delta1 + reg_term_d1   # 更新delta1
 
-    t2[:, 0] = 0
-    reg_term_d2 = (l / m) * t2
-    delta2 = delta2 + reg_term_d2
+    t2[:, 0] = 0                    # 对偏置项的theta进行0处理，不参与正则化
+    reg_term_d2 = (lambd / m) * t2  # 计算正则化项对第二层的影响
+    delta2 = delta2 + reg_term_d2   # 更新delta1
 
-    return serialize(delta1, delta2)
-
-def gradient_checking(theta, X, y, epsilon, regularized=False):
-    def a_numeric_grad(plus, minus, regularized=False):
-        """calculate a partial gradient with respect to 1 theta"""
-        if regularized:
-            return (regularized_cost(plus, X, y) - regularized_cost(minus, X, y)) / (epsilon * 2)
-        else:
-            return (cost(plus, X, y) - cost(minus, X, y)) / (epsilon * 2)
-
-    theta_matrix = expand_array(theta)  # expand to (10285, 10285)
-    epsilon_matrix = np.identity(len(theta)) * epsilon
-
-    plus_matrix = theta_matrix + epsilon_matrix
-    minus_matrix = theta_matrix - epsilon_matrix
-
-    # calculate numerical gradient with respect to all theta
-    numeric_grad = np.array([a_numeric_grad(plus_matrix[i], minus_matrix[i], regularized)
-                                    for i in range(len(theta))])
-
-    # analytical grad will depend on if you want it to be regularized or not
-    analytic_grad = regularized_gradient(theta, X, y) if regularized else gradient(theta, X, y)
-
-    # If you have a correct implementation, and assuming you used EPSILON = 0.0001
-    # the diff below should be less than 1e-9
-    # this is how original matlab code do gradient checking
-    diff = np.linalg.norm(numeric_grad - analytic_grad) / np.linalg.norm(numeric_grad + analytic_grad)
-
-    print('如果你的反向传播算法是对的，那么差异会小于 10e-9 (假设 epsilon=0.0001).\n差异为: {}\n'.format(diff))
+    return serialize(delta1, delta2)  # 将delta1和delta2序列化并返回
 
 def random_init(size):
+    '''
+    随机初始化参数
+    '''
+    # uniform函数用于从一个均匀分布中随机采样生成数据
+    # 使用均匀分布可以帮助保持初始权重的随机性，从而避免模型收敛到局部极小值
     return np.random.uniform(-0.12, 0.12, size)
 
 def nn_training(X, y):
-
+    '''
+    训练神经网络
+    '''
+    # 初始化参数
     init_theta = random_init(10285)  # 25*401 + 10*26
 
-    res = opt.minimize(fun=regularized_cost,
-                       x0=init_theta,
-                       args=(X, y, 1),
-                       method='TNC',
-                       jac=regularized_gradient,
-                       options={'maxfun': 400})
+    # 使用优化器求解参数
+    res = opt.minimize(fun=regularized_cost,      # 目标函数
+                       x0=init_theta,             # 初始参数
+                       args=(X, y, 1),            # 目标函数需要的其他参数
+                       method='TNC',              # 优化方法
+                       jac=regularized_gradient,  # 目标函数的梯度
+                       options={'maxfun': 400})   # 额外参数
     return res
 
 def show_accuracy(theta, X, y):
+    '''
+    计算神经网络的分类准确率
+    '''
+    # 调用前向传播函数，获取预测结果
     _, _, _, _, h = feed_forward(theta, X)
-
+    # 根据输出h，对每个样本进行预测，返回每个样本的预测结果
     y_pred = np.argmax(h, axis=1) + 1
 
+    # 输出分类准确率的详细信息
     print(classification_report(y, y_pred))
     
+    # 计算总体的准确率，并输出
     correct = [1 if a == b else 0 for (a, b) in zip(y_pred, y)]
     accuracy = (sum(map(int, correct)) / float(len(correct)))
     print ('总体正确率为：{0}%'.format(accuracy * 100))
@@ -262,14 +287,9 @@ def show_accuracy(theta, X, y):
 X_raw, y_raw = load_data('data2.mat', transpose=False)
 X = np.insert(X_raw, 0, np.ones(X_raw.shape[0]), axis=1)
 y = expand_y(y_raw)
-t1, t2 = load_weight('weights2.mat')
-theta = serialize(t1, t2)  # 扁平化参数，25*401+10*26=10285
 
-print(X.shape)
-print(y.shape)
-print(t1.shape)
-print(t2.shape)
-print(theta.shape)
+# print(X.shape)
+# print(y.shape)
 
 
 '''训练模型'''
